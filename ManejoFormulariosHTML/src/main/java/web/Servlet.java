7bf69c4fd3cfc86310f6c6b33a11b32d32006843
Andres <estfabiber2@hotmail.com>
2020-02-26 23:29:13 -0500

    Add files via upload

---
+++ ManejoFormulariosHTML/src/main/java/web/Servlet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author beamora
+ */
+
+@WebServlet("/servlet")
+public class Servlet extends HttpServlet{
+    
+}
